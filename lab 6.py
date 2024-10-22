d07fe62dd19e2339fb9eb3b289afb1795fc243e9
kalledus13 <kalledus13@gmail.com>
2024-10-22 12:20:42 -0400

    add my code

---
+++ lab 6.py
@@ -0,0 +1,44 @@
+#Vignesh Nanduri encoding and main
+#Lab 6 Groups 10
+def encode(password):
+    encoded_password = ''
+    for i in range(len(password)):
+        encoded_password += str((3 + int(password[i])) % 10)
+    return encoded_password
+#Artem Rospotniuk (kalledus13)
+def decode(encoded_password):
+    pass
+
+def main():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    user_option = int(input('\nPlease enter an option: '))
+    prompt = True
+    while prompt:
+        if user_option == 1:
+            password = input('Please enter your password to encode: ')
+            print('Your password has been encoded and stored!')
+            continue_menu = True
+        elif user_option == 2:
+            print(f"The encoded password is {encode(password)}, and the original password is {password}.")
+            continue_menu = True
+        elif user_option == 3:
+            prompt = False
+            continue_menu = False
+        while continue_menu:
+            if user_option == 1:
+                password = input('Please enter your password to encode: ')
+                print('Your password has been encoded and stored!')
+                continue_menu = True
+            elif user_option == 2:
+                print(f"The encoded password is {encode(password)}, and the original password is {password}.")
+                continue_menu = True
+            elif user_option == 3:
+                prompt = False
+                continue_menu = False
+
+if __name__ == '__main__':
+    main()
